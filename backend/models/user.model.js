df1cc88259bb3d53a72d83524833ad521eee7b03
Anurag-kuche <anuragkuche1@gmail.com>
2025-04-02 15:25:19 +0530

    post schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const postSchema=mongoose.Schema({
+    userId:{
+
+    },
+    body:{
+
+    },
+    likes:{
+
+    },
+    createdAt:{
+
+    },
+    updatedAt:{
+
+    },
+    media:{
+
+    },
+    active:{
+
+    },
+    fileType:{
+        
+    }
+
+
+})
