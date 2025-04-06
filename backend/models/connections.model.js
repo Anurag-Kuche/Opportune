0ce751f855493c3d0b0667b1238cf3f7cb8cc449
Anurag-kuche <anuragkuche1@gmail.com>
2025-04-06 17:35:59 +0530

    completed

--- backend/models/connections.model.js
+++ backend/models/connections.model.js
@@ -9,5 +9,12 @@ const connectionrequest=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
     },
-    
-})
+    status_accepted:{
+        type:Boolean,
+        deafult:null,
+    }
+
+});
+
+const ConnectionRequest=mongoose.model("ConnectRequest",connectionRequest);
+export default ConnectionRequest;
